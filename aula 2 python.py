5743b381adbd42b388725a40521a1a0cc8fb745c
alxsalves <85627595+alxsalves@users.noreply.github.com>
2022-05-06 12:47:17 -0300

    Add files via upload
    # a=10
    # b=5
    #primeira opcao de soma de valores
    #faremos agora interacao com usuario
    a= int(input("Digite o primeiro valor = "))
    #apos digitar, tecle enter.
    b= int(input("Digite o segundo valor = "))
    #apos digitar tecle enter.
    print(type(a))
    soma=a+b
    subtracao=a-b
    divisao=a/b
    multiplicacao=a*b
    # print(soma)
    # print(subtracao)
    # print(divisao)
    # print(multiplicacao)
    print("resultado da soma = {}".format(soma))
    print("resultado da subtracao = {}".format(subtracao))
    print("resultado da divisao = {}".format(divisao))
    print('resultado da multiplicacao = {}'.format(multiplicacao))

---
+++ aula 2 python.py
@@ -0,0 +1,21 @@
+# a=10
+# b=5
+#primeira opcao de soma de valores
+#faremos agora interacao com usuario
+a= int(input("Digite o primeiro valor = "))
+#apos digitar, tecle enter.
+b= int(input("Digite o segundo valor = "))
+#apos digitar tecle enter.
+print(type(a))
+soma=a+b
+subtracao=a-b
+divisao=a/b
+multiplicacao=a*b
+# print(soma)
+# print(subtracao)
+# print(divisao)
+# print(multiplicacao)
+print("resultado da soma = {}".format(soma))
+print("resultado da subtracao = {}".format(subtracao))
+print("resultado da divisao = {}".format(divisao))
+print('resultado da multiplicacao = {}'.format(multiplicacao))
