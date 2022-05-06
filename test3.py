46f7c8c1ba42388a9a9ac983cca180772a26f8f5
alxsalves <85627595+alxsalves@users.noreply.github.com>
2022-05-06 12:51:33 -0300

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,4 @@
+data = input ("Qual  sua data de nascimento?")
+mes = input ("Qual o seu mes de nascimento?")
+ano = input ("Qual o seu ano do seu nascimento?")
+print ("Você nasceu no dia",data,"do mes",mes,"do ano",ano,"Esta correto?")
